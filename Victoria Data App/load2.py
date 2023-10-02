9e2e4e61274186a09b856903412307edee19b314
Lniyu2 <lniyuhire_17@hotmail.com>
2023-10-02 22:18:55 +1000

    Made changes to loadData.py

--- Victoria Data App/load2.py
+++ Victoria Data App/load2.py
@@ -1,121 +1,73 @@
 import pandas as pd
 
-# Define the data types for each column based on your dataset
-data_types = {
-    "OBJECTID": object,
-    "ACCIDENT_NO": object,
-    "ABS_CODE": object,
-    "ACCIDENT_STATUS": object,
-    "ACCIDENT_DATE": object,
-    "ACCIDENT_TIME": object,
-    "ALCOHOLTIME": object,
-    "ACCIDENT_TYPE": object,
-    "DAY_OF_WEEK": object,
-    "DCA_CODE": object,
-    "HIT_RUN_FLAG": object,
-    "LIGHT_CONDITION": object,
-    "POLICE_ATTEND": object,
-    "ROAD_GEOMETRY": object,
-    "SEVERITY": object,
-    "SPEED_ZONE": object,
-    "RUN_OFFROAD": object,
-    "NODE_ID": object,
-    "LONGITUDE": object,
-    "LATITUDE": object,
-    "NODE_TYPE": object,
-    "LGA_NAME": object,
-    "REGION_NAME": object,
-    "VICGRID_X": object,
-    "VICGRID_Y": object,
-    "TOTAL_PERSONS": object,
-    "INJ_OR_FATAL": object,
-    "FATALITY": object,
-    "SERIOUSINJURY": object,
-    "OTHERINJURY": object,
-    "NONINJURED": object,
-    "MALES": object,
-    "FEMALES": object,
-    "BICYCLIST": object,
-    "PASSENGER": object,
-    "DRIVER": object,
-    "PEDESTRIAN": object,
-    "PILLION": object,
-    "MOTORIST": object,
-    "UNKNOWN": object,
-    "PED_CYCLIST_5_12": object,
-    "PED_CYCLIST_13_18": object,
-    "OLD_PEDESTRIAN": object,
-    "OLD_DRIVER": object,
-    "YOUNG_DRIVER": object,
-    "ALCOHOL_RELATED": object,
-    "UNLICENCSED": object,
-    "NO_OF_VEHICLES": object,
-    "HEAVYVEHICLE": object,
-    "PASSENGERVEHICLE": object,
-    "MOTORCYCLE": object,
-    "PUBLICVEHICLE": object,
-    "DEG_URBAN_NAME": object,
-    "DEG_URBAN_ALL": object,
-    "LGA_NAME_ALL": object,
-    "REGION_NAME_ALL": object,
-    "SRNS": object,
-    "SRNS_ALL": object,
-    "RMA": object,
-    "RMA_ALL": object,
-    "DIVIDED": object,
-    "DIVIDED_ALL": object,
-    "STAT_DIV_NAME": object
-}
-
-column_names = [
-    "OBJECTID", "ACCIDENT_NO", "ABS_CODE", "ACCIDENT_STATUS", "ACCIDENT_DATE", "ACCIDENT_TIME",
-    "ALCOHOLTIME", "ACCIDENT_TYPE", "DAY_OF_WEEK", "DCA_CODE", "HIT_RUN_FLAG", "LIGHT_CONDITION",
-    "POLICE_ATTEND", "ROAD_GEOMETRY", "SEVERITY", "SPEED_ZONE", "RUN_OFFROAD", "NODE_ID", "LONGITUDE",
-    "LATITUDE", "NODE_TYPE", "LGA_NAME", "REGION_NAME", "VICGRID_X", "VICGRID_Y", "TOTAL_PERSONS",
-    "INJ_OR_FATAL", "FATALITY", "SERIOUSINJURY", "OTHERINJURY", "NONINJURED", "MALES", "FEMALES",
-    "BICYCLIST", "PASSENGER", "DRIVER", "PEDESTRIAN", "PILLION", "MOTORIST", "UNKNOWN", "PED_CYCLIST_5_12",
-    "PED_CYCLIST_13_18", "OLD_PEDESTRIAN", "OLD_DRIVER", "YOUNG_DRIVER", "ALCOHOL_RELATED", "UNLICENCSED",
-    "NO_OF_VEHICLES", "HEAVYVEHICLE", "PASSENGERVEHICLE", "MOTORCYCLE", "PUBLICVEHICLE", "DEG_URBAN_NAME",
-    "DEG_URBAN_ALL", "LGA_NAME_ALL", "REGION_NAME_ALL", "SRNS", "SRNS_ALL", "RMA", "RMA_ALL", "DIVIDED",
-    "DIVIDED_ALL", "STAT_DIV_NAME"
-]
-
-def load_data(data_path, data_types):
+def load_data(data_path):
     try:
-        data = pd.read_csv(data_path, dtype=data_types)
+        data = pd.read_csv(data_path)
         return data
     except FileNotFoundError:
         print("File not found. Please check the file path.")
         return None
 
-def tabulate_data(data):
+def highlight_matches(val, keywords):
+    for keyword in keywords:
+        if keyword.lower() in str(val).lower():
+            return 'background-color: yellow'
+    return ''
+
+def tabulate_data(data, page_size=10, keywords=None):
     if data is not None:
-        pd.set_option('display.max_columns', None)
-        print(data)
+        pd.set_option('display.max_columns', 25)
+
+        if keywords:
+            display_data = data.head(page_size).style.applymap(lambda x: highlight_matches(x, keywords), subset=data.columns[1:])
+            print(display_data)
+        else:
+            while True:
+                print(data.head(page_size))  # Display the first 'page_size' rows
+
+                if len(data) <= page_size:
+                    break
+
+                user_input = input("Press 'Enter' to view the next page, 'q' to quit: ")
+
+                if user_input.lower() == 'q':
+                    break
+
+                data = data.iloc[page_size:]  # Move to the next page
     else:
         print("No data to tabulate.")
 
 def search_data(data, keywords):
     if data is not None:
-        filter_condition = data.apply(lambda row: any(keyword in str(row) for keyword in keywords), axis=1)
-        filtered_data = data[filter_condition]
+        # Search in column names
+        matching_columns = [col for col in data.columns if any(keyword in col for keyword in keywords)]
 
-        if not filtered_data.empty:
-            tabulate_data(filtered_data)
+        if matching_columns:
+            print("Matching columns:", matching_columns)
+            # Display the data for matching columns
+            for col_name in matching_columns:
+                print(data[col_name])
         else:
-            print("No matching entries found.")
+            print("No matching columns found.")
     else:
         print("No data to search.")
 
-# Define the path to the dataset
-dataset_path = "Crash Statistics Victoria.csv"
+def main():
+    # Define the path to the dataset
+    dataset_path = "Crash Statistics Victoria.csv"
 
-# Load the data
-accident_data = load_data(dataset_path, data_types)
+    # Load the data
+    accident_data = load_data(dataset_path)
 
-# Tabulate the data
-tabulate_data(accident_data)
+    # Get search keywords from the user
+    keywords = input("Enter search keywords (comma-separated): ").split(",")
 
-search_keywords = ["accident", "fatal"]
-search_data(accident_data, search_keywords)
+    # Tabulate the data
+    tabulate_data(accident_data, keywords=keywords)
 
+    # Perform the search in column names
+    print("Searching for keywords in column names:", keywords)
+    search_data(accident_data, keywords)
+
+if __name__ == "__main__":
+    main()
--- Victoria Data App/loadData.py
+++ Victoria Data App/loadData.py
@@ -1,127 +1,89 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-keywords = input("Enter search keywords (comma-separated): ").split(",")
+def create_bar_chart(data, labels, title, x_label, y_label):
+    plt.bar(labels, data)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
 
 
-data_types = {
-    "OBJECTID": object,
-    "ACCIDENT_NO": object,
-    "ABS_CODE": object,
-    "ACCIDENT_STATUS": object,
-    "ACCIDENT_DATE": object,
-    "ACCIDENT_TIME": object,
-    "ALCOHOLTIME": object,
-    "ACCIDENT_TYPE": object,
-    "DAY_OF_WEEK": object,
-    "DCA_CODE": object,
-    "HIT_RUN_FLAG": object,
-    "LIGHT_CONDITION": object,
-    "POLICE_ATTEND": object,
-    "ROAD_GEOMETRY": object,
-    "SEVERITY": object,
-    "SPEED_ZONE": object,
-    "RUN_OFFROAD": object,
-    "NODE_ID": object,
-    "LONGITUDE": object,
-    "LATITUDE": object,
-    "NODE_TYPE": object,
-    "LGA_NAME": object,
-    "REGION_NAME": object,
-    "VICGRID_X": object,
-    "VICGRID_Y": object,
-    "TOTAL_PERSONS": object,
-    "INJ_OR_FATAL": object,
-    "FATALITY": object,
-    "SERIOUSINJURY": object,
-    "OTHERINJURY": object,
-    "NONINJURED": object,
-    "MALES": object,
-    "FEMALES": object,
-    "BICYCLIST": object,
-    "PASSENGER": object,
-    "DRIVER": object,
-    "PEDESTRIAN": object,
-    "PILLION": object,
-    "MOTORIST": object,
-    "UNKNOWN": object,
-    "PED_CYCLIST_5_12": object,
-    "PED_CYCLIST_13_18": object,
-    "OLD_PEDESTRIAN": object,
-    "OLD_DRIVER": object,
-    "YOUNG_DRIVER": object,
-    "ALCOHOL_RELATED": object,
-    "UNLICENCSED": object,
-    "NO_OF_VEHICLES": object,
-    "HEAVYVEHICLE": object,
-    "PASSENGERVEHICLE": object,
-    "MOTORCYCLE": object,
-    "PUBLICVEHICLE": object,
-    "DEG_URBAN_NAME": object,
-    "DEG_URBAN_ALL": object,
-    "LGA_NAME_ALL": object,
-    "REGION_NAME_ALL": object,
-    "SRNS": object,
-    "SRNS_ALL": object,
-    "RMA": object,
-    "RMA_ALL": object,
-    "DIVIDED": object,
-    "DIVIDED_ALL": object,
-    "STAT_DIV_NAME": object
-}
-
-column_names = [
-    "OBJECTID", "ACCIDENT_NO", "ABS_CODE", "ACCIDENT_STATUS", "ACCIDENT_DATE", "ACCIDENT_TIME",
-    "ALCOHOLTIME", "ACCIDENT_TYPE", "DAY_OF_WEEK", "DCA_CODE", "HIT_RUN_FLAG", "LIGHT_CONDITION",
-    "POLICE_ATTEND", "ROAD_GEOMETRY", "SEVERITY", "SPEED_ZONE", "RUN_OFFROAD", "NODE_ID", "LONGITUDE",
-    "LATITUDE", "NODE_TYPE", "LGA_NAME", "REGION_NAME", "VICGRID_X", "VICGRID_Y", "TOTAL_PERSONS",
-    "INJ_OR_FATAL", "FATALITY", "SERIOUSINJURY", "OTHERINJURY", "NONINJURED", "MALES", "FEMALES",
-    "BICYCLIST", "PASSENGER", "DRIVER", "PEDESTRIAN", "PILLION", "MOTORIST", "UNKNOWN", "PED_CYCLIST_5_12",
-    "PED_CYCLIST_13_18", "OLD_PEDESTRIAN", "OLD_DRIVER", "YOUNG_DRIVER", "ALCOHOL_RELATED", "UNLICENCSED",
-    "NO_OF_VEHICLES", "HEAVYVEHICLE", "PASSENGERVEHICLE", "MOTORCYCLE", "PUBLICVEHICLE", "DEG_URBAN_NAME",
-    "DEG_URBAN_ALL", "LGA_NAME_ALL", "REGION_NAME_ALL", "SRNS", "SRNS_ALL", "RMA", "RMA_ALL", "DIVIDED",
-    "DIVIDED_ALL", "STAT_DIV_NAME"
-]
-
-def load_data(data_path, data_types):
+def load_data(data_path):
     try:
-        data = pd.read_csv(data_path, dtype=data_types)
+        data = pd.read_csv(data_path)
         return data
     except FileNotFoundError:
         print("File not found. Please check the file path.")
         return None
 
-def tabulate_data(data):
+def tabulate_data(data, page_size=10):
     if data is not None:
-        pd.set_option('display.max_columns', None)
-        print(data)
+        pd.set_option('display.max_columns', 25)
+        total_rows = len(data)
+        current_page = 1
+        while True:
+            print(data.head(page_size))  # Display the first 'page_size' rows
+            print(f"Page {current_page} of {total_rows // page_size + 1}")
+
+            if len(data) <= page_size:
+                break
+
+            user_input = input("Press 'Enter' to view the next page, 'p' for previous page, 'q' to quit: ")
+
+            if user_input.lower() == 'q':
+                break
+            elif user_input.lower() == 'p':
+                if current_page > 1:
+                    current_page -= 1
+                    data = data.iloc[:(current_page - 1) * page_size]
+                else:
+                    print("You are already on the first page.")
+            else:
+                if current_page * page_size < total_rows:
+                    current_page += 1
+                    data = data.iloc[current_page * page_size:]
+                else:
+                    print("You have reached the end of the dataset.")
     else:
         print("No data to tabulate.")
 
 def search_data(data, keywords):
     if data is not None:
-        filter_condition = data.apply(lambda row: any(keyword in str(row) for keyword in keywords), axis=1)
-        filtered_data = data[filter_condition]
+        # Search in column names
+        matching_columns = [col for col in data.columns if any(keyword in col for keyword in keywords)]
 
-        if not filtered_data.empty:
-            tabulate_data(filtered_data)
+        if matching_columns:
+            print("Matching columns:", matching_columns)
+            for col_name in matching_columns:
+                print(f"Column: {col_name}\n{data[col_name]}\n")
         else:
-            print("No matching entries found.")
+            print("No matching columns found.")
     else:
         print("No data to search.")
 
+def main():
 
-dataset_path = "Crash Statistics Victoria.csv"
+    data = [10, 20, 30, 40, 50]
+    labels = ['Category A', 'Category B', 'Category C', 'Category D', 'Category E']
 
+    create_bar_chart(data, labels, 'Sample Bar Chart', 'Categories', 'Values')
 
-accident_data = load_data(dataset_path, data_types)
+    # Define the path to the dataset
+    dataset_path = "Crash Statistics Victoria.csv"
 
+    # Load the data
+    accident_data = load_data(dataset_path)
 
-tabulate_data(accident_data)
+    # Tabulate the data
+    tabulate_data(accident_data)
 
+    # Get search keywords from the user
+    keywords = input("Enter search keywords (comma-separated): ").split(",")
 
-keywords = input("Enter search keywords (comma-separated): ").split(",")
+    # Perform the search in column names
+    print("Searching for keywords in column names:", keywords)
+    search_data(accident_data, keywords)
 
-
-search_data(accident_data, keywords)
-
-
+if __name__ == "__main__":
+    main()
